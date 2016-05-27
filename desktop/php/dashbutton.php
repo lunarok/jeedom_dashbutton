5a5a86f85ea26ee6f0dd3d8e2a78ea8bf9cfb2e3
Cédric Dupont <lunarok@gmail.com>
2016-05-27 14:22:56 +0200

    Update and rename dashbutton.js to dashbutton.php

---
+++ desktop/php/dashbutton.php
@@ -0,0 +1,204 @@
+<?php
+
+if (!isConnect('admin')) {
+    throw new Exception('{{401 - Accès non autorisé}}');
+}
+sendVarToJS('eqType', 'dashbutton');
+$eqLogics = eqLogic::byType('dashbutton');
+state = config::byKey('include_mode', 'dashbutton');
+echo '<div id="div_inclusionAlert"></div>';
+if ($state == 1) {
+			echo '<div class="alert jqAlert alert-warning" id="div_inclusionAlert" style="margin : 0px 5px 15px 15px; padding : 7px 35px 7px 15px;">{{Vous Ãªtes en mode inclusion. Cliquez Ã  nouveau sur le bouton d\'inclusion pour sortir de ce mode}}</div>';
+		}
+?>
+
+<div class="row row-overflow">
+    <div class="col-lg-2 col-md-3 col-sm-4">
+        <div class="bs-sidebar">
+            <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
+            <?php
+           if ($state == 1) {
+               echo ' <a class="btn btn-success tooltips changeIncludeState" title="{{Inclure Dashb Button}}" data-state="0" style="width : 100%;margin-bottom : 5px;"><i class="fa fa-sign-in fa-rotate-90"></i> {{ArrÃªter inclusion}}</a>';
+           } else {
+             echo ' <a class="btn btn-default tooltips changeIncludeState" title="{{Inclure Dash Button}}" data-state="1" style="width : 100%;margin-bottom : 5px;"><i class="fa fa-sign-in fa-rotate-90"></i> {{Mode inclusion}}</a>';
+           }
+           ?>
+                <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
+                <?php
+                foreach ($eqLogics as $eqLogic) {
+                    echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
+
+    <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+   <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
+   <div class="eqLogicThumbnailContainer">
+   <?php
+     if ($state == 1) {
+       echo '<div class="cursor changeIncludeState card" data-state="0" style="background-color : #8000FF; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
+       echo '<center>';
+       echo '<i class="fa fa-sign-in fa-rotate-90" style="font-size : 5em;color:#94ca02;"></i>';
+       echo '</center>';
+       echo '<span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>{{ArrÃªter inclusion}}</center></span>';
+       echo '</div>';
+     } else {
+       echo '<div class="cursor changeIncludeState card" data-state="1" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
+       echo '<center>';
+       echo '<i class="fa fa-sign-in fa-rotate-90" style="font-size : 5em;color:#94ca02;"></i>';
+       echo '</center>';
+       echo '<span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>{{Mode inclusion}}</center></span>';
+       echo '</div>';
+     }
+     ?>
+     <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+     <center>
+       <i class="fa fa-wrench" style="font-size : 5em;color:#767676;"></i>
+     </center>
+     <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
+   </div>
+ </div>
+
+
+        <legend><i class="fa fa-table"></i>  {{Mes équipements dashbutton}} </legend>
+        <div class="eqLogicThumbnailContainer">
+
+                <?php
+                foreach ($eqLogics as $eqLogic) {
+                  $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
+                    echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff ; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
+                    echo "<center>";
+                    echo '<img src="plugins/dashbutton/doc/images/dashbutton_icon.png" height="105" width="95" />';
+                    echo "</center>";
+                    echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+                    echo '</div>';
+                }
+                ?>
+            </div>
+    </div>
+
+
+    <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+        <div class="row">
+            <div class="col-sm-6">
+                <form class="form-horizontal">
+            <fieldset>
+                <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i>  {{Général}}
+                <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i>
+                </legend>
+                <div class="form-group">
+                    <label class="col-md-2 control-label">{{dashbutton}}</label>
+                    <div class="col-md-3">
+                        <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                        <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement dashbutton}}"/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-2 control-label" >{{Objet parent}}</label>
+                    <div class="col-md-3">
+                        <select class="form-control eqLogicAttr" data-l1key="object_id">
+                            <option value="">{{Aucun}}</option>
+                            <?php
+                            foreach (object::all() as $object) {
+                                echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+                            }
+                            ?>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-2 control-label">{{Catégorie}}</label>
+                    <div class="col-md-8">
+                        <?php
+                        foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+                            echo '<label class="checkbox-inline">';
+                            echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
+                            echo '</label>';
+                        }
+                        ?>
+
+                    </div>
+                </div>
+                <div class="form-group">
+                <label class="col-sm-2 control-label" ></label>
+                <div class="col-sm-9">
+                 <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
+                  <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
+                </div>
+                </div>
+                <div class="form-group expertModeVisible">
+                    <label class="col-md-2 control-label">{{Délai max entre 2 messages}}</label>
+                    <div class="col-md-8">
+                        <input class="eqLogicAttr form-control" data-l1key="timeout" placeholder="Délai maximum autorisé entre 2 messages (en mn)"/>
+                    </div>
+                </div>
+                            <div class="form-group">
+                    <label class="col-sm-2 control-label">{{Commentaire}}</label>
+                    <div class="col-md-8">
+                        <textarea class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="commentaire" ></textarea>
+                    </div>
+                </div>
+
+            </fieldset>
+
+        </form>
+        </div>
+
+                <div id="infoNode" class="col-sm-6">
+                <form class="form-horizontal">
+                    <fieldset>
+                        <legend><i class="fa fa-info-circle"></i>  {{Configuration}}</legend>
+
+                        <div class="form-group">
+                          <label class="col-md-2 control-label">{{MAC Dash Button}}</label>
+                      		<div class="col-md-3">
+                          	<span class="eqLogicAttr" data-l1key="configuration" data-l2key="uid"></span>
+                      		</div>
+                        </div>
+
+                      <div class="form-group">
+                        <label class="col-md-2 control-label">{{Dernière Activité}}</label>
+                    		<div class="col-md-3">
+                        	<span class="eqLogicAttr" data-l1key="configuration" data-l2key="updatetime"></span>
+                    		</div>
+                      </div>
+
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+
+	<legend><i class="fa fa-table"></i>  {{Informations}}</legend>
+
+        <table id="table_cmd" class="table table-bordered table-condensed">
+            <thead>
+                <tr>
+                    <th style="width: 50px;">#</th>
+                    <th style="width: 200px;">{{Scanner}}</th>
+                    <th style="width: 150px;">{{Valeur}}</th>
+                    <th style="width: 150px;">{{Source}}</th>
+                    <th style="width: 100px;">{{Paramètres}}</th>
+                    <th style="width: 100px;"></th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+
+        <form class="form-horizontal">
+            <fieldset>
+                <div class="form-actions">
+                    <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+                    <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+                </div>
+            </fieldset>
+        </form>
+
+    </div>
+</div>
+
+<?php include_file('desktop', 'dashbutton', 'js', 'dashbutton'); ?>
+<?php include_file('core', 'plugin.template', 'js'); ?>
