e6fd6e01aa6f98d09181e24179aa9e5358bfdbce
Cédric Dupont <lunarok@gmail.com>
2018-03-16 17:48:28 +0100

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -18,14 +18,10 @@
 
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
-function dashbutton_install() {
-    $sensor_path = dirname(__FILE__) . '/../node';
-		exec('cd ' . $sensor_path . '; rm -rf node_modules');
-}
-
 function dashbutton_update() {
-    $sensor_path = dirname(__FILE__) . '/../node';
-		exec('cd ' . $sensor_path . '; rm -rf node_modules');
+    dashbutton::deamon_stop();
+    $sensor_path = dirname(__FILE__) . '/../resources';
+    exec('cd ' . $sensor_path . '; rm -rf node_modules');
 }
 
 ?>
