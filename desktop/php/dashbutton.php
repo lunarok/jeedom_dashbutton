f2ab405961a9a5d3c71510b94fbd9916271558e8
lunarok <lunarok@gmail.com>
2019-07-22 14:47:30 +0200

    Compliancy v4

--- desktop/php/dashbutton.php
+++ desktop/php/dashbutton.php
@@ -104,7 +104,7 @@ $eqLogics = eqLogic::byType('dashbutton');
                 <select class="form-control eqLogicAttr" data-l1key="object_id">
                   <option value="">{{Aucun}}</option>
                   <?php
-                  foreach (object::all() as $object) {
+                  foreach (jeeObject::all() as $object) {
                     echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
                   }
                   ?>
